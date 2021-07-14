5c99bd92453fdd7461bec4bf8f343227528dd1c5
Henrique Ramos <henriqueramos0106@gmail.com>
2021-07-13 23:44:21 -0300

    Segunda aula da imersão alura

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,36 @@
+import React, { useState } from 'react'
 import { MainGrid } from '../src/components/MainGrid'
 import { Box } from '../src/components/Box'
-import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons'
+import { AlurakutMenu, OrkutNostalgicIconSet, AlurakutProfileSidebarMenuDefault } from '../src/lib/AlurakutCommons'
 import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations';
 
 function ProfileSideBar(propriedades) {
   return (
     <Box>
-      <img src={`https://github.com/${propriedades.githubUser}.png`}  style={{ borderRadius: '8px' }}/>
+      <img src={`https://github.com/${propriedades.githubUser}.png`}  style={{ borderRadius: '8px' }} />
+      
+      <hr />
+
+      <p>
+        <a className="boxLink" href={`https://github.com/${propriedades.githubUser}.png`}>
+          @{propriedades.githubUser}
+        </a>
+      </p>
+
+      <hr />
+
+      <AlurakutProfileSidebarMenuDefault />
     </Box>
   )
 }
 
 export default function Home() {
+  const [comunidades, setComunidades] = useState([{
+    id: '1234568749540',
+    title: 'Eu odeio acordar cedo',
+    image: 'https://alurakut.vercel.app/capa-comunidade-01.jpg'
+  }])
+
   const usuarioAleatorio = 'hrammos'
   const pessoasFavoritas = [
     'juunegreiros',
@@ -21,6 +40,22 @@ export default function Home() {
     'marcobrunodev',
     'felipefialho',
   ]
+
+  const handleCriarComunidade = (event) => {
+    event.preventDefault()
+
+    const dadosFormulario = new FormData(event.target)
+
+    const comunidade = {
+      id: new Date().toISOString(),
+      title: dadosFormulario.get('title'),
+      image: dadosFormulario.get('image'),
+    }
+    
+    const comunidadesAtualizadas = [...comunidades, comunidade];
+    
+    setComunidades(comunidadesAtualizadas)
+  }
 
   return (
     <>
@@ -36,6 +71,32 @@ export default function Home() {
             </h1>
 
             <OrkutNostalgicIconSet />
+          </Box>
+
+          <Box>
+            <h2 className="subTitle">O que você deseja fazer?</h2>
+            
+            <form onSubmit={handleCriarComunidade}>
+              <div>
+                <input 
+                  placeholder="Qual vai ser o nome da sua comunidade?" 
+                  name="title" 
+                  aria-label="Qual vai ser o nome da sua comunidade?"
+                  type="text"
+                />
+              </div>
+              <div>
+                <input 
+                  placeholder="Coloque uma URL para usarmos de capa" 
+                  name="image" 
+                  aria-label="Coloque uma URL para usarmos de capa"
+                />
+              </div>
+
+              <button type="submit">
+                Criar comunidade
+              </button>
+            </form>
           </Box>
         </div>
         <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea' }}>
@@ -57,9 +118,24 @@ export default function Home() {
               })}
             </ul>
           </ProfileRelationsBoxWrapper>
-          <Box>
-            Comunidades
-          </Box>
+          <ProfileRelationsBoxWrapper>
+            <h2 className="smallTitle">
+              Comunidades ({comunidades.length})
+            </h2>
+
+            <ul>
+              {comunidades.map((itemAtual) => {
+                return (
+                  <li key={itemAtual.id}>
+                    <a href={`/users/${itemAtual.title}`}>
+                      <img src={itemAtual.image} />
+                      <span>{itemAtual.title}</span>
+                    </a>
+                  </li>
+                )
+              })}
+            </ul>
+          </ProfileRelationsBoxWrapper>
         </div>
       </MainGrid>
     </>
